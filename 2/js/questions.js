988d4821195e77bcf933d5b536ca3901bfa3c71d
tunlinmg <60735111+tunlinmg@users.noreply.github.com>
2023-03-09 14:54:59 +0630

    Create questions.js

---
+++ 2/js/questions.js
@@ -0,0 +1,141 @@
+const quizQuestions = [
+    {
+        id: 1,
+        question: "Which method removes the last element from an array and returns that element?",
+        choices: [
+            "pop()",
+            "shift()",
+        ],
+        answer: "pop()"
+    },
+    {
+        id: 2,
+        question: "Which method adds a new item to the end of an array?",
+        choices: [
+            "push()",
+            "pop()"
+        ],
+        answer: "push()"
+    },
+/*    {
+        id: 3,
+        question: "Which method creates a new array based on a selection?",
+        choices: [
+            "shift()",
+            "appendChild()",
+            "slice()",
+            "push()"
+        ],
+        answer: "slice()"
+    },
+    {
+        id: 4,
+        question: "Which method adds a new item to the beginning of an array?",
+        choices: [
+            "unshift()",
+            "pop()",
+            "shift()",
+            "slice()"
+        ],
+        answer: "unshift()"
+    },
+    {
+        id: 5,
+        question: "Which method removes the first element from an array and returns that element?",
+        choices: [
+            "push()",
+            "slice()",
+            "unshift()",
+            "shift()"
+        ],
+        answer: "shift()"
+    },
+    {
+        id: 6,
+        question: "Which method rearranges the elements of an array into alphabetical or numerical order?",
+        choices: [
+            "every()",
+            "reduce()",
+            "sort()",
+            "includes()"
+        ],
+        answer: "sort()"
+    },
+    {
+        id: 7,
+        question: "Which method creates a new array, populated with the results of calling a provided function on every element in the calling array?",
+        choices: [
+            "map()",
+            "filter()",
+            "reduce()",
+            "forEach()"
+        ],
+        answer: "map()"
+    },
+    {
+        id: 8,
+        question: "Which method takes in an array, performs a function that you provide on each element in that array, and returns one single value?",
+        choices: [
+            "filter()",
+            "forEach()",
+            "map()",
+            "reduce()"
+        ],
+        answer: "reduce()"
+    },
+    {
+        id: 9,
+        question: "Which method performs a function that you provide on each element in that array, and returns only those elements that pass the test implemented by that function?",
+        choices: [
+            "every()",
+            "filter()",
+            "map()",
+            "forEach()"
+        ],
+        answer: "filter()"
+    },
+    {
+        id: 10,
+        question: "Which method executes a provided function once for each element in an array?",
+        choices: [
+            "every()",
+            "forEach()",
+            "map()",
+            "reduce()"
+        ],
+        answer: "forEach()"
+    },
+    {
+        id: 11,
+        question: "Which method tests whether all elements in the array pass the test implemented by the provided function, returning a boolean (true/false) value?",
+        choices: [
+            "map()",
+            "every()",
+            "forEach()",
+            "includes()"
+        ],
+        answer: "every()"
+    },
+    {
+        id: 12,
+        question: "Which method determines whether an array contains a certain value among its entries, returning true or false as appropriate?",
+        choices: [
+            "every()",
+            "includes()",
+            "indexOf()",
+            "pop()"
+        ],
+        answer: "includes()"
+    },
+    {
+        id: 13,
+        question: "Which method returns the first index at which a given element can be found in the array (or -1 if it is not present)?",
+        choices: [
+            "indexOf()",
+            "includes()",
+            "slice()",
+            "map()"
+        ],
+        answer: "indexOf()"
+    } */
+];
